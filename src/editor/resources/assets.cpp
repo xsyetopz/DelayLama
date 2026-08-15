6b92306c1366eada503527f64a929d66e1615d07
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2026-08-15 09:44:52 +0300

    i hate you windows i hate you windows i hate you windows

--- src/editor/resources/assets.cpp
+++ src/editor/resources/assets.cpp
@@ -1,44 +1,43 @@
 #include "assets.hpp"
 
-#include <array>
 #include <cstddef>
 #include <span>
 
 namespace {
 
     template<std::size_t extent>
-    consteval auto png_view(const std::array<unsigned char, extent>& bytes) noexcept
+    consteval auto png_view(const unsigned char (&bytes)[extent]) noexcept
         -> std::span<const unsigned char> {
-        return std::span<const unsigned char> {bytes}.first(extent - 1U);
+        return std::span<const unsigned char> {bytes, extent - 1U};
     }
 
-    const auto control_panel_png_data = std::to_array<unsigned char>({
+    const unsigned char control_panel_png_data[] = {
 #include "control_panel.png.h"
-    });
-    const auto help_panel_png_data = std::to_array<unsigned char>({
+    };
+    const unsigned char help_panel_png_data[] = {
 #include "help_panel.png.h"
-    });
-    const auto monk_sprite_sheet_png_data = std::to_array<unsigned char>({
+    };
+    const unsigned char monk_sprite_sheet_png_data[] = {
 #include "monk_sprite_sheet.png.h"
-    });
-    const auto knob_strip_a_png_data = std::to_array<unsigned char>({
+    };
+    const unsigned char knob_strip_a_png_data[] = {
 #include "knob_strip_a.png.h"
-    });
-    const auto knob_strip_b_png_data = std::to_array<unsigned char>({
+    };
+    const unsigned char knob_strip_b_png_data[] = {
 #include "knob_strip_b.png.h"
-    });
-    const auto scene_background_png_data = std::to_array<unsigned char>({
+    };
+    const unsigned char scene_background_png_data[] = {
 #include "scene_background.png.h"
-    });
-    const auto ui_tile_a_png_data = std::to_array<unsigned char>({
+    };
+    const unsigned char ui_tile_a_png_data[] = {
 #include "ui_tile_a.png.h"
-    });
-    const auto ui_tile_b_png_data = std::to_array<unsigned char>({
+    };
+    const unsigned char ui_tile_b_png_data[] = {
 #include "ui_tile_b.png.h"
-    });
-    const auto ui_arrow_png_data = std::to_array<unsigned char>({
+    };
+    const unsigned char ui_arrow_png_data[] = {
 #include "ui_arrow.png.h"
-    });
+    };
 
 }
 namespace delay_lama_assets {
